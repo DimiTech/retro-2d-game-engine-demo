e00280aa9c2cbc01d8f8a15e20a127227a58914e
Dušan Dimitrić <dusan_dimitric@yahoo.com>
2018-12-23 12:46:45 +0100

    Update the demo

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 
 rm -rf ./build/*                            && \
 cp ../retro-2d-game-engine/build/* ./build/ && \
+cp ../retro-2d-game-engine/index.html .     && \
 git commit -am "Update the demo"            && \
 git push

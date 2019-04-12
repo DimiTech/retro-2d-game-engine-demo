a8e561283c90ae788fa86fef9c19c51cc47172a6
Dušan Dimitrić <dusan_dimitric@yahoo.com>
2019-04-12 21:34:55 +0200

    Update the demo

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@
 rm -rf ./build/*                            && \
 cp ../retro-2d-game-engine/build/* ./build/ && \
 cp ../retro-2d-game-engine/index.html .     && \
+rsync -r ../retro-2d-game-engine/audio ./   && \
 git commit -am "Update the demo"            && \
 git push

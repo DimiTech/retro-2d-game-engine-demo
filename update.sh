e13d5694c2ed57efdeb33234de45b87d58ffc5c6
Dusan Dimitric <dusan@mitipi.com>
2019-08-23 16:49:57 +0200

    Update the demo

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-rm -rf ./build/*                            && \
-cp ../retro-2d-game-engine/build/* ./build/ && \
-cp ../retro-2d-game-engine/index.html .     && \
-rsync -r ../retro-2d-game-engine/audio ./   && \
-git add ./audio                             && \
-git commit -am "Update the demo"            && \
+rm -rf ./build/*                             && \
+cp ../retro-2d-game-engine/build/* ./build/  && \
+cp ../retro-2d-game-engine/index.html .      && \
+rsync -r ../retro-2d-game-engine/audio ./    && \
+rsync -r ../retro-2d-game-engine/graphics ./ && \
+git add ./audio                              && \
+git commit -am "Update the demo"             && \
 git push

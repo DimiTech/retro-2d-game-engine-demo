2b826dd2122e731a92f9cc812ef49bb4cdfd8b0c
dusandimitric <dusan_dimitric@yahoo.com>
2022-10-16 12:44:38 +0200

    Update the demo

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ cp ../retro-2d-game-engine/build/* ./build/  && \
 cp ../retro-2d-game-engine/index.html .      && \
 rsync -r ../retro-2d-game-engine/audio ./    && \
 rsync -r ../retro-2d-game-engine/graphics ./ && \
-git add ./audio                              && \
+git add ./audio ./graphics                   && \
 git commit -am "Update the demo"             && \
 git push
